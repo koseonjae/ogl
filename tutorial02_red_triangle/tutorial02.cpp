8b7d49ac92b8b1c571859530b9f95b4c48a781ae
koseonjae <goop702@gmail.com>
2019-09-25 01:04:48 +0900

    add file prefix

--- tutorial02_red_triangle/tutorial02.cpp
+++ tutorial02_red_triangle/tutorial02.cpp
@@ -61,7 +61,7 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader", "../tutorial02_red_triangle/");
 
 
 	static const GLfloat g_vertex_buffer_data[] = { 
