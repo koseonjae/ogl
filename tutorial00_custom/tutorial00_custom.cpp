c739e6b4fe1aec28529eb63fd1761d2b6915921b
koseonjae <goop702@gmail.com>
2019-10-29 01:42:23 +0900

    tutorial 5, texture

--- tutorial00_custom/tutorial00_custom.cpp
+++ tutorial00_custom/tutorial00_custom.cpp
@@ -1,22 +1,23 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
+#include <common/controls.hpp>
 #include <common/shader.hpp>
 #include <common/BmpLoader.hpp>
-#include <common/controls.hpp>
-
-GLFWwindow *window;
 
 using namespace std;
 using namespace glm;
+
+GLFWwindow *window{ nullptr };
 
 static const GLfloat g_vertex_buffer_data[] = { -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f };
 static const GLfloat g_uv_buffer_data[] = { 0.000059f, 1.0f - 0.000004f, 0.000103f, 1.0f - 0.336048f, 0.335973f, 1.0f - 0.335903f, 1.000023f, 1.0f - 0.000013f, 0.667979f, 1.0f - 0.335851f, 0.999958f, 1.0f - 0.336064f, 0.667979f, 1.0f - 0.335851f, 0.336024f, 1.0f - 0.671877f, 0.667969f, 1.0f - 0.671889f, 1.000023f, 1.0f - 0.000013f, 0.668104f, 1.0f - 0.000013f, 0.667979f, 1.0f - 0.335851f, 0.000059f, 1.0f - 0.000004f, 0.335973f, 1.0f - 0.335903f, 0.336098f, 1.0f - 0.000071f, 0.667979f, 1.0f - 0.335851f, 0.335973f, 1.0f - 0.335903f, 0.336024f, 1.0f - 0.671877f, 1.000004f, 1.0f - 0.671847f, 0.999958f, 1.0f - 0.336064f, 0.667979f, 1.0f - 0.335851f, 0.668104f, 1.0f - 0.000013f, 0.335973f, 1.0f - 0.335903f, 0.667979f, 1.0f - 0.335851f, 0.335973f, 1.0f - 0.335903f, 0.668104f, 1.0f - 0.000013f, 0.336098f, 1.0f - 0.000071f, 0.000103f, 1.0f - 0.336048f, 0.000004f, 1.0f - 0.671870f, 0.336024f, 1.0f - 0.671877f, 0.000103f, 1.0f - 0.336048f, 0.336024f, 1.0f - 0.671877f, 0.335973f, 1.0f - 0.335903f, 0.667969f, 1.0f - 0.671889f, 1.000004f, 1.0f - 0.671847f, 0.667979f, 1.0f - 0.335851f };
 
 int main( void )
 {
+    // GLFW
+
     if( !glfwInit() )
     {
         assert( false );
@@ -26,14 +27,11 @@ int main( void )
     glfwWindowHint( GLFW_SAMPLES, 4 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-
     window = glfwCreateWindow( 1024, 768, "tutorial00", nullptr, nullptr );
     glfwMakeContextCurrent( window );
-
     glfwSetInputMode( window, GLFW_STICKY_KEYS, GL_TRUE );
-    glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
-    glfwPollEvents();
-    glfwSetCursorPos( window, 1024 / 2, 768 / 2 );
+
+    // GLEW
 
     glewExperimental = GL_TRUE;
     if( glewInit() != GLEW_OK )
@@ -41,43 +39,41 @@ int main( void )
         assert( false );
     }
 
-    glClearColor( 0, 0, 0.5, 0 );
+    // GL
 
-    GLuint programId = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader", "../tutorial00_custom/" );
+    GLuint programId, vao, vbo, uv, mvpLocation, samplerLocation, textureId;
 
-    GLuint vao;
+    programId = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader", "../tutorial00_custom/" );
+
     glGenVertexArrays( 1, &vao );
     glBindVertexArray( vao );
 
-    GLuint vertexBuffer;
-    glGenBuffers( 1, &vertexBuffer );
-    glBindBuffer( GL_ARRAY_BUFFER, vertexBuffer );
+    glGenBuffers( 1, &vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
     glBufferData( GL_ARRAY_BUFFER, sizeof( g_vertex_buffer_data ), g_vertex_buffer_data, GL_STATIC_DRAW );
 
-    GLuint uvBuffer;
-    glGenBuffers( 1, &uvBuffer );
-    glBindBuffer( GL_ARRAY_BUFFER, uvBuffer );
+    glGenBuffers( 1, &uv );
+    glBindBuffer( GL_ARRAY_BUFFER, uv );
     glBufferData( GL_ARRAY_BUFFER, sizeof( g_uv_buffer_data ), g_uv_buffer_data, GL_STATIC_DRAW );
 
-    vector<unsigned char> image = BmpLoader::loadBmp( "../tutorial00_custom/input.bmp" );
-    GLuint textureId;
+    mvpLocation = glGetUniformLocation( programId, "MVP" );
+    mat4 model = mat4( 1.f );
+    mat4 view = lookAt( vec3( 5, 5, 5 ), vec3( 0, 0, 0 ), vec3( 0, 1, 0 ) );
+    mat4 projection = perspective( radians( 90.f ), ( GLfloat ) 1024 / ( GLfloat ) 768, 0.1f, 100.f );
+    mat4 mvp = projection * view * model;
+
+    samplerLocation = glGetUniformLocation( programId, "sampler" );
+
+    auto image = BmpLoader::loadBmp( "../tutorial00_custom/input.bmp" );
     glGenTextures( 1, &textureId );
     glBindTexture( GL_TEXTURE_2D, textureId );
     glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, 300, 300, 0, GL_BGR, GL_UNSIGNED_BYTE, image.data() );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
 
-    // sampler in a program represents a single texture of a particular texture type.
-    // The type of the sampler corresponds to the type of the texture that can be used by that sampler.
-    // 셰이더에서 texture에 접근할 수 있게하는 매개변수
-    GLuint sampler = glGetUniformLocation( programId, "sampler" );
-
-    glEnable( GL_CULL_FACE );
-
-    glEnable( GL_DEPTH_TEST );
-    glDepthFunc( GL_LESS );
+    // EVENTS
 
     do
     {
@@ -85,31 +81,25 @@ int main( void )
 
         glUseProgram( programId );
 
-        computeMatricesFromInputs();
-        glm::mat4 ProjectionMatrix = getProjectionMatrix();
-        glm::mat4 ViewMatrix = getViewMatrix();
-        glm::mat4 ModelMatrix = glm::mat4( 1.0 );
-        glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-        GLuint mvpHandle = glGetUniformLocation( programId, "MVP" );
-        glUniformMatrix4fv( mvpHandle, 1, GL_FALSE, &MVP[0][0] );
+        glUniformMatrix4fv( mvpLocation, 1, GL_FALSE, &mvp[0][0] );
 
-        glBindTexture( GL_TEXTURE_2D, textureId );
         glActiveTexture( GL_TEXTURE0 );
-        glUniform1i( sampler, 0 ); // glsl에서 sampler가 0번째 texture에 접근할 수 있도록 한다.
+        glBindTexture( GL_TEXTURE_2D, textureId );
+        glUniform1i( samplerLocation, 0 );
 
         glEnableVertexAttribArray( 0 );
-        glEnableVertexAttribArray( 1 );
-
-        glBindBuffer( GL_ARRAY_BUFFER, vertexBuffer );
+        glBindBuffer( GL_ARRAY_BUFFER, vbo );
         glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
-
-        glBindBuffer( GL_ARRAY_BUFFER, uvBuffer );
-        glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, nullptr );
-
         glDrawArrays( GL_TRIANGLES, 0, 3 * 12 );
+
+        glEnableVertexAttribArray( 1 );
+        glBindBuffer( GL_ARRAY_BUFFER, uv );
+        glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, nullptr );
+        glDrawArrays( GL_TRIANGLES, 0, 2 * 18 );
 
         glDisableVertexAttribArray( 0 );
         glDisableVertexAttribArray( 1 );
+
 
         glfwSwapBuffers( window );
         glfwPollEvents();
