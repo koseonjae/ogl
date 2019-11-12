17cc5ef0c009cd0b68744292f40131fa2da859a9
koseonjae <goop702@gmail.com>
2019-11-13 00:37:59 +0900

    tangent, bitangent by myself

--- common/tangentspace.cpp
+++ common/tangentspace.cpp
@@ -25,17 +25,15 @@ void computeTangentBasis(
 		glm::vec2 & uv1 = uvs[i+1];
 		glm::vec2 & uv2 = uvs[i+2];
 
-		// Edges of the triangle : postion delta
-		glm::vec3 deltaPos1 = v1-v0;
-		glm::vec3 deltaPos2 = v2-v0;
+        glm::vec3 deltaPos1 = v1 - v0;
+        glm::vec3 deltaPos2 = v2 - v0;
 
-		// UV delta
-		glm::vec2 deltaUV1 = uv1-uv0;
-		glm::vec2 deltaUV2 = uv2-uv0;
+        glm::vec2 deltaUV1 = uv1 - uv0;
+        glm::vec2 deltaUV2 = uv2 - uv0;
 
-		float r = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV1.y * deltaUV2.x);
-		glm::vec3 tangent = (deltaPos1 * deltaUV2.y   - deltaPos2 * deltaUV1.y)*r;
-		glm::vec3 bitangent = (deltaPos2 * deltaUV1.x   - deltaPos1 * deltaUV2.x)*r;
+        float r = ( deltaUV1.x * deltaUV2.y - deltaUV1.y * deltaUV2.x );
+        glm::vec3 tangent = ( deltaPos1 * deltaUV2.y - deltaPos2 * deltaUV1.y ) / r;
+        glm::vec3 bitangent = ( -deltaPos1 * deltaUV2.x + deltaPos2 * deltaUV1.x ) / r;
 
 		// Set the same tangent for all three vertices of the triangle.
 		// They will be merged later, in vboindexer.cpp
