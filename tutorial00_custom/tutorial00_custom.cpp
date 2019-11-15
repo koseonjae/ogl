afb1eb4a32d715455de6af9098237319c22a92bd
koseonjae <goop702@gmail.com>
2019-11-15 19:53:54 +0900

    tutorial 2, triangle

--- tutorial00_custom/tutorial00_custom.cpp
+++ tutorial00_custom/tutorial00_custom.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <common/shader.hpp>
 
 using namespace std;
 
 GLFWwindow *window{ nullptr };
 
 int g_width{ 1024 }, g_height{ 768 };
+GLfloat g_triangle[]{ -1, -1, 0, 1, -1, 0, 0, 1, 0 };
 
 int main( void )
 {
@@ -30,15 +32,38 @@ int main( void )
         assert( false );
     }
 
+    // GL
+
     glClearColor( 0, 0, 0.4, 0 );
+
+    GLuint programId = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader", "../tutorial00_custom/" );
+
+    GLuint vertexArray;
+    glGenVertexArrays( 1, &vertexArray );
+    glBindVertexArray( vertexArray );
+
+    GLuint vertexBuffer;
+    glGenBuffers( 1, &vertexBuffer );
+    glBindBuffer( GL_ARRAY_BUFFER, vertexBuffer );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( g_triangle ), g_triangle, GL_STATIC_DRAW );
 
     do
     {
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
+        glUseProgram( programId );
+
+        glEnableVertexAttribArray( 0 );
+        glBindBuffer( GL_ARRAY_BUFFER, vertexBuffer );
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
+
+        glDisableVertexAttribArray( 0 );
+
         glfwSwapBuffers( window );
         glfwPollEvents();
     } while( glfwGetKey( window, GLFW_KEY_ESCAPE ) != GL_TRUE && !glfwWindowShouldClose( window ) );
-    
+
     return 0;
 }
