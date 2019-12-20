daaf0e1496bd9fdbde838019fbbe845b5fcafb62
koseonjae <goop702@gmail.com>
2019-12-20 16:00:10 +0900

    tutorial 16, shadow mapping

--- tutorial00_custom/tutorial00_custom.cpp
+++ tutorial00_custom/tutorial00_custom.cpp
@@ -61,7 +61,7 @@ int main( void )
     vector<vec3> vertices;
     vector<vec2> uvs;
     vector<vec3> normals;
-    loadOBJ( "../tutorial07_model_loading/cube.obj", vertices, uvs, normals );
+    loadOBJ( "../tutorial16_shadowmaps/room_thickwalls.obj", vertices, uvs, normals );
 
     vector<unsigned short> indices;
     vector<vec3> indexed_vertices;
@@ -93,27 +93,111 @@ int main( void )
     glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, elementbuffer );
     glBufferData( GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof( unsigned short ), indices.data(), GL_STATIC_DRAW );
 
-    GLuint MatrixID = glGetUniformLocation( programID, "MVP" );
+    GLuint mvpLocation = glGetUniformLocation( programID, "MVP" );
+    GLuint ShadowSamplerLocation = glGetUniformLocation( programID, "shadowSampler" );
+    GLuint diffuseSamplerLocation = glGetUniformLocation( programID, "diffuseSampler" );
+    GLuint DepthBiasMVPLocation = glGetUniformLocation( programID, "DepthBiasMVP" );
+    GLuint diffuseTextureId = loadDDS( "../tutorial16_shadowmaps/uvmap.DDS" );
 
-    GLuint diffuseTextureID = glGetUniformLocation( programID, "diffuseSampler" );
-    GLuint diffuseTexture = loadDDS( "../tutorial07_model_loading/uvmap.DDS" );
+    // ----------------------------
+    // depth variables
+    // ----------------------------
+
+    GLuint depthProgramId = LoadShaders( "Depth.vertexshader", "Depth.fragmentshader", "../tutorial00_custom/" );
+
+    GLuint depthFramebufferId;
+    glGenFramebuffers( 1, &depthFramebufferId );
+    glBindFramebuffer( GL_FRAMEBUFFER, depthFramebufferId );
+
+    GLuint depthTextureId;
+    glGenTextures( 1, &depthTextureId );
+    glBindTexture( GL_TEXTURE_2D, depthTextureId );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, 1024, 1024, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_R_TO_TEXTURE );
+
+    glFramebufferTexture( GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTextureId, 0 );
+
+    glDrawBuffer( GL_NONE );
+
+    if( glCheckFramebufferStatus( GL_FRAMEBUFFER ) != GL_FRAMEBUFFER_COMPLETE )
+        return false;
+
+    GLuint depthMVPLocation = glGetUniformLocation( depthProgramId, "DepthMVP" );
 
     do
     {
+        // ----------------------------
+        // depth frame buffer rendering
+        // ----------------------------
+
+        glBindFramebuffer( GL_FRAMEBUFFER, depthProgramId );
+        glViewport( 0, 0, 1024, 1024 );
+
+        // We don't use bias in the shader, but instead we draw back faces,
+        // which are already separated from the front faces by a small distance
+        // (if your geometry is made this way)
+        glEnable( GL_CULL_FACE );
+        glCullFace( GL_BACK ); // Cull back-facing triangles -> draw only front-facing triangles
+
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        glUseProgram( depthProgramId );
+
+        glm::vec3 lightInvDir = glm::vec3( 0.5f, 2, 2 );
+        glm::mat4 depthProjectionMatrix = glm::ortho<float>( -10, 10, -10, 10, -10, 20 );
+        glm::mat4 depthViewMatrix = glm::lookAt( lightInvDir, glm::vec3( 0, 0, 0 ), glm::vec3( 0, 1, 0 ) );
+        glm::mat4 depthModelMatrix = glm::mat4( 1.0 );
+        glm::mat4 depthMVP = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
+
+        glUniformMatrix4fv( depthMVPLocation, 1, GL_FALSE, &depthMVP[0][0] );
+
+        glEnableVertexAttribArray( 0 );
+        glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, elementbuffer );
+        glDrawElements( GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, nullptr );
+
+        glDisableVertexAttribArray( 0 );
+
+        // ----------------------------
+        // main frame buffer rendering
+        // ----------------------------
+
+        glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+        glViewport( 0, 0, windowWidth, windowHeight );
+
+        glEnable( GL_CULL_FACE );
+        glCullFace( GL_BACK ); // Cull back-facing triangles -> draw only front-facing triangles
+
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
         glUseProgram( programID );
 
         glActiveTexture( GL_TEXTURE0 );
-        glBindTexture( GL_TEXTURE_2D, diffuseTexture );
-        glUniform1i( diffuseTextureID, 0 );
+        glBindTexture( GL_TEXTURE_2D, diffuseTextureId );
+        glUniform1i( diffuseSamplerLocation, 0 );
+
+        glActiveTexture( GL_TEXTURE1 );
+        glBindTexture( GL_TEXTURE_2D, depthTextureId );
+        glUniform1i( ShadowSamplerLocation, 1 );
 
         computeMatricesFromInputs( g_width, g_height );
         mat4 model = mat4( 1.f );
         mat4 view = getViewMatrix();
         mat4 projection = getProjectionMatrix();
-        mat4 mvp = projection * view * model;
-        glUniformMatrix4fv( MatrixID, 1, GL_FALSE, &mvp[0][0] );
+        mat4 MVP = projection * view * model;
+
+        glm::mat4 biasMatrix( 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.5, 0.5, 1.0 );
+        glm::mat4 depthBiasMVP = biasMatrix * depthMVP;
+
+        glUniformMatrix4fv( mvpLocation, 1, GL_FALSE, &MVP[0][0] );
+        glUniformMatrix4fv( DepthBiasMVPLocation, 1, GL_FALSE, &depthBiasMVP[0][0] );
 
         glEnableVertexAttribArray( 0 );
         glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
@@ -127,6 +211,7 @@ int main( void )
         glBindBuffer( GL_ARRAY_BUFFER, normalbuffer );
         glVertexAttribPointer( 2, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
 
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, elementbuffer );
         glDrawElements( GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, nullptr );
 
         glDisableVertexAttribArray( 0 );
