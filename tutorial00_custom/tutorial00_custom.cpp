a38a3b40b99a0d55d2359534b3452ca8fb384ad6
koseonjae <goop702@gmail.com>
2019-12-20 16:58:00 +0900

    모델 로드하는 부분까지만 다시 작성해봤음. shadow mapping 로직 추가해보자

--- tutorial00_custom/tutorial00_custom.cpp
+++ tutorial00_custom/tutorial00_custom.cpp
@@ -51,8 +51,103 @@ int main( void )
 
     // GL
 
+    glEnable( GL_DEPTH_TEST );
+    glDepthFunc( GL_LESS );
+    glEnable( GL_CULL_FACE );
+    glCullFace( GL_BACK );
+
+    glClearColor( 0, 0, 0.4, 0 );
+
+    GLuint programId = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader", "../tutorial00_custom/" );
+
+    vector<vec3> vertices;
+    vector<vec2> uvs;
+    vector<vec3> normals;
+    loadOBJ( "../tutorial16_shadowmaps/room_thickwalls.obj", vertices, uvs, normals );
+
+    vector<unsigned short> indices;
+    vector<vec3> indexed_vertices;
+    vector<vec2> indexed_uvs;
+    vector<vec3> indexed_normals;
+    indexVBO( vertices, uvs, normals, indices, indexed_vertices, indexed_uvs, indexed_normals );
+
+    GLuint vertexarray;
+    glGenVertexArrays( 1, &vertexarray );
+    glBindVertexArray( vertexarray );
+
+    GLuint vertexbuffer;
+    glGenBuffers( 1, &vertexbuffer );
+    glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
+    glBufferData( GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof( vec3 ), indexed_vertices.data(), GL_STATIC_DRAW );
+
+    GLuint uvbuffer;
+    glGenBuffers( 1, &uvbuffer );
+    glBindBuffer( GL_ARRAY_BUFFER, uvbuffer );
+    glBufferData( GL_ARRAY_BUFFER, indexed_uvs.size() * sizeof( vec2 ), indexed_uvs.data(), GL_STATIC_DRAW );
+
+    GLuint normalbuffer;
+    glGenBuffers( 1, &normalbuffer );
+    glBindBuffer( GL_ARRAY_BUFFER, normalbuffer );
+    glBufferData( GL_ARRAY_BUFFER, indexed_normals.size() * sizeof( vec3 ), indexed_normals.data(), GL_STATIC_DRAW );
+
+    GLuint elementbuffer;
+    glGenBuffers( 1, &elementbuffer );
+    glBindBuffer( GL_ARRAY_BUFFER, elementbuffer );
+    glBufferData( GL_ARRAY_BUFFER, indices.size() * sizeof( unsigned short ), indices.data(), GL_STATIC_DRAW );
+
+    GLuint diffuseTextureId = loadDDS( "../tutorial16_shadowmaps/uvmap.DDS" );
+
+    GLuint mvpLocation = glGetUniformLocation( programId, "MVP" );
+
+    GLuint diffuseSamplerLocation = glGetUniformLocation( programId, "diffuseSampler" );
+
     do
     {
+        // -----------------
+        // depth frame buffer
+        // -----------------
+
+        // note: later
+
+        // -----------------
+        // main frame buffer
+        // -----------------
+
+        glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        glUseProgram( programId );
+
+        computeMatricesFromInputs( windowWidth, windowHeight );
+        mat4 model = mat4( 1.f );
+        mat4 view = getViewMatrix();
+        mat4 projection = getProjectionMatrix();
+        mat4 MVP = projection * view * model;
+        glUniformMatrix4fv( mvpLocation, 1, GL_FALSE, &MVP[0][0] );
+
+        glActiveTexture( GL_TEXTURE0 );
+        glBindTexture( GL_TEXTURE_2D, diffuseTextureId );
+        glUniform1i( diffuseSamplerLocation, 0 );
+
+        glEnableVertexAttribArray( 0 );
+        glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer );
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+
+        glEnableVertexAttribArray( 1 );
+        glBindBuffer( GL_ARRAY_BUFFER, uvbuffer );
+        glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, nullptr );
+
+        glEnableVertexAttribArray( 2 );
+        glBindBuffer( GL_ARRAY_BUFFER, normalbuffer );
+        glVertexAttribPointer( 2, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, elementbuffer );
+        glDrawElements( GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, nullptr );
+
+        glDisableVertexAttribArray( 0 );
+        glDisableVertexAttribArray( 1 );
+        glDisableVertexAttribArray( 2 );
 
         glfwSwapBuffers( window );
         glfwPollEvents();
