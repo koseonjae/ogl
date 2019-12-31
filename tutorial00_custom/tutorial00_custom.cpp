97a35f19b954ff9689ede3a17be596844fb8f576
koseonjae <goop702@gmail.com>
2019-12-31 23:38:10 +0900

    substitute dds with bmp

--- tutorial00_custom/tutorial00_custom.cpp
+++ tutorial00_custom/tutorial00_custom.cpp
@@ -72,7 +72,16 @@ int main( void )
     glBindBuffer( GL_ARRAY_BUFFER, uvbuffer );
     glBufferData( GL_ARRAY_BUFFER, sizeof( g_uv_buffer_data ), g_uv_buffer_data, GL_STATIC_DRAW );
 
-    GLuint diffuseTextureId = loadDDS("../tutorial05_textured_cube/uvtemplate.DDS");
+    auto image = BmpLoader::loadBmp( "../tutorial00_custom/input.bmp" );
+
+    GLuint diffuseTextureId;
+    glGenTextures( 1, &diffuseTextureId );
+    glBindTexture( GL_TEXTURE_2D, diffuseTextureId );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, 300, 300, 0, GL_BGR, GL_UNSIGNED_BYTE, image.data() );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
 
     GLuint diffuseTextureLocation = glGetUniformLocation( programID, "diffuseSampler" );
 
